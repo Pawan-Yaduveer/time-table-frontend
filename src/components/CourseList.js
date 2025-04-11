39a99eb7203919fdd5221771ab8387d043c9c357
Pawan  Kumar <90625035+Pawan-Yaduveer@users.noreply.github.com>
2025-04-11 12:21:49 +0530

    Update CourseList.js

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,1 +1,30 @@
 
+import React, { useEffect, useState } from "react";
+import { fetchCourses } from "../api";
+
+function CourseList({ refreshTrigger }) {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const loadCourses = async () => {
+      const data = await fetchCourses();
+      setCourses(data);
+    };
+    loadCourses();
+  }, [refreshTrigger]);
+
+  return (
+    <div>
+      <h3>Course List</h3>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.code}>
+            {course.code} - {course.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CourseList;
