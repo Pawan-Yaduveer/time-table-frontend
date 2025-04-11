f0253b3ffddea5ac96cbac1fdf66e92e42d126b7
Pawan  Kumar <90625035+Pawan-Yaduveer@users.noreply.github.com>
2025-04-11 12:24:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 
+import React, { useState } from "react";
+import CourseForm from "./components/CourseForm";
+import CourseList from "./components/CourseList";
+import TimeSlotList from "./components/TimeSlotList";
+
+function App() {
+  const [refresh, setRefresh] = useState(false);
+
+  const triggerRefresh = () => {
+    setRefresh(!refresh);
+  };
+
+  return (
+    <div className="App">
+      <h1>Timetable Management</h1>
+      <CourseForm onAdd={triggerRefresh} />
+      <CourseList refreshTrigger={refresh} />
+      <TimeSlotList />
+    </div>
+  );
+}
+
+export default App;
